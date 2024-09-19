99eaf947914a1af3484a955f47301d3435b3564d
Rodrigo A <r.agredaipar@gmail.com>
2024-09-19 04:37:31 -0500

    feat(backend): implementacion de los DTOS de expert y habitType

--- src/main/java/com/healthy/service/impl/AdminExpertServiceImpl.java
+++ src/main/java/com/healthy/service/impl/AdminExpertServiceImpl.java
@@ -1,5 +1,9 @@
 package com.healthy.service.impl;
 
+import com.healthy.dto.ExpertDTO;
+import com.healthy.exception.BadRequestException;
+import com.healthy.exception.ResourceNotFoundException;
+import com.healthy.mapper.ExpertMapper;
 import com.healthy.model.entity.Expert;
 import com.healthy.repository.ExpertRepository;
 import com.healthy.service.AdminExpertService;
@@ -9,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.lang.module.ResolutionException;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,48 +21,73 @@ import java.util.List;
 @Service
 public class AdminExpertServiceImpl implements AdminExpertService {
     private final ExpertRepository expertRepository;
+    private final ExpertMapper expertMapper;
 
     @Transactional(readOnly = true)
     @Override
-    public List<Expert> getAll() {
-        return expertRepository.findAll();
+    public List<ExpertDTO> getAll() {
+        List<Expert> experts = expertRepository.findAll();
+        return experts.stream()
+                .map(expertMapper::toDTO)
+                .toList();
     }
 
     @Transactional(readOnly = true)
     @Override
-    public Page<Expert> paginate(Pageable pageable) {
-        return expertRepository.findAll(pageable);
+    public Page<ExpertDTO> paginate(Pageable pageable) {
+        Page<Expert> experts = expertRepository.findAll(pageable);
+        return experts.map(expertMapper::toDTO);
     }
 
     @Transactional(readOnly = true)
     @Override
-    public Expert findById(Integer id) {
-        return expertRepository.findById(id).
-                orElseThrow(() -> new RuntimeException("Expert not found"));
+    public ExpertDTO findById(Integer id) {
+        Expert expert = expertRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("El experto con ID " + id + " no existe."));
+        return expertMapper.toDTO(expert);
     }
 
     @Transactional
     @Override
-    public Expert create(Expert expert) {
+    public ExpertDTO create(ExpertDTO expertDTO) {
+        expertRepository.findByFirstNameAndLastName(expertDTO.getFirstName(), expertDTO.getLastName())
+                        .ifPresent(existingExpert -> {
+                            throw new BadRequestException("El experto ya existe con el mismo nombre y apellido.");
+                        });
+
+        Expert expert = expertMapper.toEntity(expertDTO);
         expert.setCreatedAt(LocalDateTime.now());
-        return expertRepository.save(expert);
+        expert = expertRepository.save(expert);
+        return expertMapper.toDTO(expert);
     }
 
     @Transactional
     @Override
-    public Expert update(Integer id, Expert updateExpert) {
-        Expert expertFromDb = findById(id);
-        expertFromDb.setFirstName(updateExpert.getFirstName());
-        expertFromDb.setLastName(updateExpert.getLastName());
-        expertFromDb.setBio(updateExpert.getBio());
+    public ExpertDTO update(Integer id, ExpertDTO updateExpertDTO) {
+        Expert expertFromDb = expertRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El experto con ID " + id + " no existe."));
+
+        expertRepository.findByFirstNameAndLastName(updateExpertDTO.getFirstName(), updateExpertDTO.getLastName())
+                .filter(existingExpert -> !existingExpert.getId().equals(id))
+                .ifPresent(existingExpert -> {
+                    throw new BadRequestException("Ya existe un experto con el mismo nombre y apellido");
+                });
+
+        expertFromDb.setFirstName(updateExpertDTO.getFirstName());
+        expertFromDb.setLastName(updateExpertDTO.getLastName());
+        expertFromDb.setBio(updateExpertDTO.getBio());
+        expertFromDb.setExpertise(updateExpertDTO.getExpertise());
         expertFromDb.setUpdatedAt(LocalDateTime.now());
-        return expertRepository.save(expertFromDb);
+
+        expertFromDb = expertRepository.save(expertFromDb);
+        return expertMapper.toDTO(expertFromDb);
     }
 
     @Transactional
     @Override
     public void delete(Integer id) {
-        Expert expert = findById(id);
+        Expert expert = expertRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("El experto con ID " + id + " no existe."));
         expertRepository.delete(expert);
     }
 }
